1a7d1bf04129ea1cd9ad92baecccdf44af9d833d
Pravallika Kambhampati <pravallika@upcalibre.com>
2021-06-18 20:08:00 +0530

    updated end point

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ const app = require('express')();
 const PORT = 8080;
 
 const quotes = require("./db.json");
-
+var index = 0;
 
 app.listen(
     PORT,
     () => console.log(`it's alive on http://localhost:${PORT}`) 
 )
 
+
+
 app.get('/quotes', (req, res) =>  {
 
-    var index = Math.floor(Math.random() * (200 - 0) + 0);
-    console.log(index);
+    // var index = Math.floor(Math.random() * (200 - 0) + 0);
+    // console.log(index);
 
     res.status(200).send({
         quotes: quotes[index]
     })
+    console.log(index);
+    index = index +1;
+
 })
 
 
