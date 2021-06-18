a984ebd7549b6d5fe5dd2702e9c9c11b03d330fb
Pravallika Kambhampati <pravallika@upcalibre.com>
2021-06-18 20:20:40 +0530

    cleaning up!

--- index.js
+++ index.js
@@ -9,19 +9,12 @@ app.listen(
     () => console.log(`it's alive on http://localhost:${PORT}`) 
 )
 
-
-
 app.get('/quotes', (req, res) =>  {
-
-    // var index = Math.floor(Math.random() * (200 - 0) + 0);
-    // console.log(index);
 
     res.status(200).send({
         quotes: quotes[index]
     })
-    console.log(index);
     index = index +1;
-
 })
 
 
